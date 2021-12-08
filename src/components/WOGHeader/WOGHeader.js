b6bef561f469e6edc6768a7a8faef66473325007
Samuel Lee <samuel.lwy@live.com.sg>
2021-12-09 00:16:12 +0800

    Add: WOG Header with Singapore Lion

---
+++ src/components/WOGHeader/WOGHeader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './WOGHeader.css'
+
+const WOGHeader = () => {
+    return (
+        <div class='header'>
+            <div class='header-content'>
+                <img src="./images/singapore-lion.png" />
+                <span>An Official Website of the <span class="strong">Singapore Government </span></span>
+            </div>
+        </div>
+    )
+}
+
+export default WOGHeader

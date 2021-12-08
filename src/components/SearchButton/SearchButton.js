fb6eb5717464d06974355de2ab6e877fa66e87ee
Samuel Lee <samuel.lwy@live.com.sg>
2021-12-08 13:24:30 +0800

    Inital setup: SearchButton and SearchTextBox Components

---
+++ src/components/SearchButton/SearchButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchButton.css'
+
+const SearchButton = () => {
+    return (
+        <button>
+            <i class="fas fa-search"></i> Search
+        </button>
+    )
+}
+
+export default SearchButton
---
+++ src/components/SearchTextBox/SearchTextBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchTextBox.css'
+
+const SearchTextBox = () => {
+    return (
+        <input type="text">
+
+        </input>
+    )
+}
+
+export default SearchTextBox

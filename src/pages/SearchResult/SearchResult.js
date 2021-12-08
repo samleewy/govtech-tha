581a8296cb722ce1c59d5a673de6cd8c2c7d1352
Samuel Lee <samuel.lwy@live.com.sg>
2021-12-09 00:56:52 +0800

    Add: Logic for Search Result page

--- src/pages/SearchResult/SearchResult.js
+++ src/pages/SearchResult/SearchResult.js
@@ -1,35 +1,56 @@
-import React from 'react'
-import SearchButton from '../../components/SearchButton/SearchButton'
+import React, { useState } from 'react'
+import SearchControl from '../../components/SearchControl/SearchControl'
 import SearchEntry from '../../components/SearchEntry/SearchEntry'
-import SearchTextBox from '../../components/SearchTextBox/SearchTextBox'
+import WOGHeader from '../../components/WOGHeader/WOGHeader'
+import { highlightText } from '../../utils/highlight'
 import './SearchResult.css'
 
 const SearchResult = () => {
+
+    const [searchQeury, setSearchQuery] = useState("")
+    const [results, setResults] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+
     return (
-        <div class='container'>
-            <div class='top-bar'>
-                <div class='top-bar-content'>
-                    <div style={{display: 'flex'}}>
-                        <SearchTextBox />
-                        <SearchButton />
-                    </div>
+        <div className='container'>
+            <WOGHeader />
+            <div className='top-bar'>
+                <div className='top-bar-content'>
+                        <SearchControl 
+                            style={{width: '100%', height: 0, display: 'flex'}}
+                            searchQuery={searchQeury}
+                            setSearchQuery={setSearchQuery}
+                            results={results}
+                            setResults={setResults}
+                            setLoading={setLoading}
+                            setError={setError} />
                 </div>
             </div>
-            <div class='search-results'>
-                <div class='search-results-info'>
-                    Showing 1-10 of 300 results
+            {error && <div className='error-panel'>
+                Your search met with an accident, try again please :(
+            </div>}
+            {loading && !error && <div className='loading-panel'>
+                We are loading your search results, please wait! :D
+            </div>}
+            { results && !loading && <div className='search-results'>
+                <div className='search-results-info'>
+                    Showing {((results.Page - 1) * 10) + 1}
+                    -
+                    {results.Page * results.PageSize} of {results.TotalNumberOfResults} results
                 </div>
-                <div class='search-results-content'>
-                    <SearchEntry 
-                        style={{display: 'flex'}}
-                        title="Buy an HDB Flat - LifeSG"
-                        link="https://services.life.gov.sg/government-services/buy-hdb/"/>
-                    <SearchEntry 
-                        style={{display: 'flex'}}
-                        title="Buy an HDB Flat - LifeSG"
-                        link="https://services.life.gov.sg/government-services/buy-hdb/"/>
+                <div className='search-results-content'>
+                    {results.ResultItems && results.ResultItems.length > 0 && results.ResultItems.map(result => {
+                        return (
+                        <SearchEntry 
+                            key={result.DocumentId || result.Id}
+                            title={highlightText(result.DocumentTitle.Highlights, result.DocumentTitle.Text)} 
+                            desc={highlightText(result.DocumentExcerpt.Highlights, result.DocumentExcerpt.Text)}
+                            link={result.DocumentURI} />
+                        )
+                    })}
                 </div>
-            </div>
+            </div> }
         </div>
     )
 }

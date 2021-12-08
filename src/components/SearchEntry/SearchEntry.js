a0394683fce0bb4399981d1b75da66c48aa2d42b
Samuel Lee <samuel.lwy@live.com.sg>
2021-12-08 23:33:59 +0800

    Update: SearchEntry component with actual variables

--- src/components/SearchEntry/SearchEntry.js
+++ src/components/SearchEntry/SearchEntry.js
@@ -4,16 +4,14 @@ import './SearchEntry.css'
 const SearchEntry = ({ title, desc, link }) => {
     return (
         <div>
-            <div class='title'>
+            <div className='title'>
                 <a href={link}>{ title }</a>
             </div>
-            <div class='desc'>
-                1 Sep 2021 — Plan Your <span class='highlight'>Finances</span> Applying for a Flat From 
-                HDB Before You Apply What to Expect Steps Involved Buying an HDB Resale Flat Eligibility 
-                Criteria What to Expect...
+            <div className='desc'>
+                1 Sep 2021 — { desc }
             </div>
-            <div class='link'>
-                { link }
+            <div className='link'>
+                <a href={ link }>{ link }</a>
             </div>
         </div>
     )

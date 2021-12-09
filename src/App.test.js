02159bc1464866f7aa3fe7f2495fd83a81ec9a67
Samuel Lee <samuel.lwy@live.com.sg>
2021-12-09 15:34:29 +0800

    Update: App component and include test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("This suit is to test the App component", () => {
+  it('should render correctly', () => {
+    const { asFragment } = render(<App />)
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
